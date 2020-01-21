32948c8066a879e2dd82defeb42ddf8ded04afdd
george-penkov <ulitkoved66@gmail.com>
2020-01-21 23:30:01 +0300

    upd2

--- clients/src/main/resources/static/app.js
+++ clients/src/main/resources/static/app.js
@@ -1,45 +1,36 @@
-/*"use strict";
-$(document).ready(function(){
-    $('form > input').keyup(function() {
-
-            var empty = false;
-            $('form > input').each(function() {
-                if ($(this).val() == '') {
-                    empty = true;
-                }
-            });
-
-            if (empty) {
-                $('#submitBtn').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-            } else {
-                $('#submitBtn').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
-                $("#submitBtn").removeClass("btn-secondary");
-                $("#submitBtn").addClass("btn-warning");
-            }
-        });
-
-    });
-*/
-<script>
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-</script>
-
-$(".chzn-select").chosen()
+"use strict";
 // Define your client-side logic here.
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+function sendMessage(ip, port, command, district, requestText){
+    var address = `${ip}:${port}/${command}`;
+    var request = new XMLHttpRequest();
+    request.open("PUT", address, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.timeout = 15000;
+    request.send(JSON.stringify(data));
+
+}
+
+document.getElementById("submitBtn").onclick = () => {
+
+    var district = document.getElementById('adminDistrict').value
+        requestText = document.getElementById('request').value;
+    sendMessage("127.0.0.1","8080", "sendMessage", district, requestText);
+};
