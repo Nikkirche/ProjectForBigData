34163ed642ea583ef83a882631950ab15d2f1d84
george-penkov <ulitkoved66@gmail.com>
2020-01-26 22:10:55 +0300

    upd

--- clients/src/main/resources/static/app.js
+++ clients/src/main/resources/static/app.js
@@ -1,10 +1,12 @@
 "use strict";
 // Define your client-side logic here.
+$(document).ready(function(){
+
     $("#jkhTags").hide();
     $("#blagoTags").hide();
     $("#illegalTags").hide();
     $("#transTags").hide();
-
+    $("#forma").hide();
 
     $("#tag").on("change", function(){
         switch( $("#tag").val() ) {
@@ -12,28 +14,28 @@
                 $("#tagSelect").removeClass("col-md-12");
                 $("#tagSelect").addClass("col-md-4");
                 $(".podTags:visible").hide();
-                $("#jkhTags").show(700);
+                $("#jkhTags").show(300);
                 break;
 
             case "Blago":
                 $("#tagSelect").removeClass("col-md-12");
                 $("#tagSelect").addClass("col-md-4");
                 $(".podTags:visible").hide();
-                $("#blagoTags").show(700);
+                $("#blagoTags").show(300);
                 break;
 
             case "Illegal":
                 $("#tagSelect").removeClass("col-md-12");
                 $("#tagSelect").addClass("col-md-4");
                 $(".podTags:visible").hide();
-                $("#illegalTags").show(700);
+                $("#illegalTags").show(300);
                 break;
 
             case "Transport":
                 $("#tagSelect").removeClass("col-md-12");
                 $("#tagSelect").addClass("col-md-4");
                 $(".podTags:visible").hide();
-                $("#transTags").show(700);
+                $("#transTags").show(300);
                 break;
 
             case "Other":
@@ -43,47 +45,60 @@
         }
     });
 
- $('.inpt').on('keyup change', function(){
-      if( $("#adminDistrict").val().length != 0 && $("#request").val().length > 30){
-            $("#submitBtn").removeAttr("disabled");
-            $("#submitBtn").removeClass("btn-outline-secondary");
-            $("#submitBtn").addClass("btn-primary");
-        }
-      else{
-            $("#submitBtn").prop("disabled", true);
-            $("#submitBtn").removeClass("btn-primary");
-            $("#submitBtn").addClass("btn-outline-secondary");
-      }
+     $('.inpt').on('keyup change', function(){
+          if( $("#adminDistrict").val() != null && $("#request").val().length > 30 && $("#tag").val() != null){
+                $("#submitBtn").removeClass("disabledbutton");
+                $("#submitBtn").removeClass("btn-outline-secondary");
+                $("#submitBtn").addClass("btn-success");
+            }
+          else{
+                $("#submitBtn").prop("disabled", true);
+                $("#submitBtn").removeClass("btn-success");
+                $("#submitBtn").addClass("btn-outline-secondary");
+          }
      });
 
-function getMethod(address){
-    $.get(`/${address}`,  // url
-      function (data, textStatus, jqXHR) {  // success callback
-          console.log('status: ' + textStatus + ', data:' + data);
+    function getMethod(address){
+        $.get(`/${address}`,  // url
+          function (data, textStatus, jqXHR) {  // success callback
+              console.log('status: ' + textStatus + ', data:' + data);
+        });
+    };
+
+    function postMethod(address, data){
+        $.post(`/${address}`, data)
+          .done(function( requestData ) {
+            alert( "Data Loaded: " + requestData );
+          });
+    };
+
+    $('#nextBtn').on('click', function(){
+        $("#privet").fadeOut(500, function(){
+            $("#forma").fadeIn(200);
+        });
     });
-}
 
-function postMethod(address, data){
-    $.post(`/${address}`, data)
-      .done(function( requestData ) {
-        alert( "Data Loaded: " + requestData );
-      });
-}
+    $('#backBtn').on('click', function(){
+            $("#forma").fadeOut(500, function(){
+                $("#privet").fadeIn(200);
+            });
+        });
 
-$('#submitBtn').on('click', function(){
-    console.log('clicked')
-    var data = {
-        district: $('#adminDistrict').val(),
-        requestText: $('#request').val(),
-        nadTag: $($('#seltag').find('div:visible select')[0]).val(),
-        podTag: $($('#seltag').find('div:visible select')[1]).val()
-    }
-    //postMethod('postData', data);
-    var result = getMethod('getData');
-    console.log(result)
+    $('#submitBtn').on('click', function(){
+        console.log('clicked')
+        var data = {
+            district: $('#adminDistrict').val(),
+            requestText: $('#request').val(),
+            nadTag: $($('#seltag').find('div:visible select')[0]).val(),
+            podTag: $($('#seltag').find('div:visible select')[1]).val()
+        }
+        alert(JSON.stringify(data))
+        postMethod('postData', data);
+        var result = getMethod('getData');
+        console.log(result)
 
-})
-
+    })
+});
 
 
 
