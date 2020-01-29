e1fc11c1dde5f6febf618da54c76562bf8646bf0
visitor <ulitkoved66@gmail.com>
2020-01-29 15:09:52 +0300

    update tables

--- clients/src/main/resources/static/data.js
+++ clients/src/main/resources/static/data.js
@@ -14,19 +14,39 @@ function create(t,attr,parent_node,innerdata){
         });
     };
 window.onload = function () {
-        var d = getMethod('getData');
- var tb= document.getElementById('inputTable')
-        f = {"created_time":1,"permalink_url":1,"id":1,"shares":1}
-        d['data'].forEach(function(val){
+       // var d = getMethod('getData');
+       // var d = require('./ok.json');
+
+       $.getJSON("./ok.json", function(json) {
+           console.log(json);
+           var tb= document.getElementById('inputTable')
+           f = {"id":1,"administration":1,"filled":1,"subfilled":1,"appear":1,"date":1}
+           json.forEach(function(val){
                 tr_dom = create('tr',{},tb,'')
                 Object.keys(f).forEach(function(tr){
                         if(tr in  val){
-                                if('shares' == tr)
-                                        td_dom = create('td',{},tr_dom,val[tr]['count'])
-                                else
-                                        td_dom = create('td',{},tr_dom,val[tr])
+                                td_dom = create('td',{},tr_dom,val[tr])
                         }else
                                 td_dom = create('td',{},tr_dom,'')
                 })
-        })
+            })
+        });
      };
+$(document).ready(function () {
+  $('#dtMaterialDesignExample').DataTable();
+  $('#dtMaterialDesignExample_wrapper').find('label').each(function () {
+    $(this).parent().append($(this).children());
+  });
+  $('#dtMaterialDesignExample_wrapper .dataTables_filter').find('input').each(function () {
+    const $this = $(this);
+    $this.attr("placeholder", "Search");
+    $this.removeClass('form-control-sm');
+  });
+  $('#dtMaterialDesignExample_wrapper .dataTables_length').addClass('d-flex flex-row');
+  $('#dtMaterialDesignExample_wrapper .dataTables_filter').addClass('md-form');
+  $('#dtMaterialDesignExample_wrapper select').removeClass('custom-select custom-select-sm form-control form-control-sm');
+  $('#dtMaterialDesignExample_wrapper select').addClass('mdb-select');
+   $('#dtMaterialDesignExample_wrapper .mdb-select').materialSelect();
+
+  $('#dtMaterialDesignExample_wrapper .dataTables_filter').find('label').remove();
+});
